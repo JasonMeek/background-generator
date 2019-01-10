2451c125185645077f4cc237e937c21aa5311b45
Jason Meek <32433296+JasonMeek@users.noreply.github.com>
2019-01-10 16:38:16 +0200

    adding starting project

---
+++ script.js
@@ -0,0 +1,41 @@
+var css = document.querySelector("h3");
+var colour1 = document.querySelector(".colour1");
+var colour2 = document.querySelector(".colour2");
+var body = document.getElementById("gradient");
+var random = document.querySelector("button");
+
+
+function startup(){
+    body.style.background = "linear-gradient(to right, " + colour1.value + ", " + colour2.value + ")";
+    css.textContent = body.style.background + ";";
+}
+
+function setGradient() {
+    body.style.background = 
+    "linear-gradient(to right, " 
+    + colour1.value 
+    + ", " 
+    + colour2.value 
+    + ")";
+
+    css.textContent = body.style.background + ";";
+}
+
+function generateRandomBackground() {
+    body.style.background = 
+    "linear-gradient(to right, " 
+    + "#"+((1<<24)*Math.random()|0).toString(16) 
+    + ", " 
+    + "#"+((1<<24)*Math.random()|0).toString(16)
+    + ")";
+
+    css.textContent = body.style.background + ";";
+}
+
+startup();
+
+colour1.addEventListener("input", setGradient);
+
+colour2.addEventListener("input", setGradient);
+
+random.addEventListener("click", generateRandomBackground);
